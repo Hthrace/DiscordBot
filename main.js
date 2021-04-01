ebe5ca17970b22feaa652db2ff60b250293f8887
Hailey Thrace <hthrace@gmail.com>
2021-04-01 06:47:26 -0400

    Warning command added with rework to where user validation is called from

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
 const { client } = require("./config/discordClient")
 const argsHandler = require("./bin/argsHandler");
-const { argsFinder, comandPrefixValid } = argsHandler
-const userValidation = require("./bin/userValidation");
-const { userValid } = userValidation;
+const { argsFinder, comandPrefixValid } = argsHandler;
+const { moderator } = require("./config/permissions");
 
 mongoose.connect('mongodb://localhost/DiscordBot', {
     useNewUrlParser: true,
@@ -25,15 +24,20 @@ client.on('ready', () => {
 
 client.on('message', async msg => {
     /* const user = client.user
-    const guild = client.guilds.cache.get('282786877847764992')
+    const guild = client.guilds.cache.get(msg.guild.id)
     const member = guild.members.fetch("282786181958074368")
     console.log((await member).voice.setMute(true)) */
-
+    //console.log(client.guilds.cache.get(msg.guild.id))
+    //console.log(client.ws.ping)
     try {
-        if (!msg.author.bot && comandPrefixValid(msg) === "$") {
-            userValid(argsFinder(msg), msg);
+        const guild = client.guilds.cache.get(msg.guild.id)
+        const member = guild.members.fetch(msg.author.id)
+        const mod = (await member).roles.cache.has(moderator);
+        if (!msg.author.bot && mod && comandPrefixValid(msg) === "$") {
+            return argsFinder(msg);
         }
     } catch (err) {
+        console.log(err)
         return
     }
 });
